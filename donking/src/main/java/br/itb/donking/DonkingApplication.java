da81e2315d352ce8b986778a00e4a66a62e82ac5
KaikyRMendes <krodriguesmendes@gmail.com>
2023-06-19 22:58:38 -0300

    Projeto criado no Sts

--- donking/src/main/java/br/itb/donking/DonkingApplication.java
+++ donking/src/main/java/br/itb/donking/DonkingApplication.java
@@ -1,4 +1,4 @@
-package com.donking.donking;
+package br.itb.donking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- donking/src/test/java/br/itb/donking/DonkingApplicationTests.java
+++ donking/src/test/java/br/itb/donking/DonkingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.donking.donking;
+package br.itb.donking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
